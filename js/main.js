82915573b74d4bd18edac53ff176966c174144d0
Kairat <kairt72@gmail.com>
2023-12-22 12:27:13 +0600

    Final-fantasy2

--- js/main.js
+++ js/main.js
@@ -21,3 +21,30 @@ accordionButtons.forEach((btn) => {
     this.classList.toggle("is-open");
   });
 });
+
+var swiper = new Swiper(".mySwiper", {
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: true,
+  },
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+  },
+});
